e4edb0aca7a616afa03cd09c48cad9c1eac9108a
darele <darcai009@gmail.com>
2020-06-09 09:06:13 -0600

    plantilla pa concursos

---
+++ base/b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector <int>
+#define vl vector <ll>
+#define dl pair <ll, ll>
+#define ii pair <int, int>
+#define vii vector <ii>
+#define graph vector <vi>
+#define inf 1000000000
+ 
+using namespace std;
+ 
+bool comp(int a, int b) {
+	return a > b;
+}
+ 
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+
+	return 0;
+}
