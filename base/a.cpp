0d352ea51484a5f6c1bd701ce90c62a432ddb3c2
darele <darcai009@gmail.com>
2020-09-19 20:22:54 -0600

    actualizar base

--- base/a.cpp
+++ base/a.cpp
@@ -6,9 +6,9 @@
 #define ii pair <int, int>
 #define vii vector <ii>
 #define graph vector <vi>
-#define REP(a, b) for (int i = a, i < b; i++)
-#define REP2(a, b) for (int j = a, j < b; j++)
-#define REPS(a, b, c) for (int i = a, i < b; i += c)
+#define REP(a, b) for (int i = a; i < b; i++)
+#define REP2(a, b) for (int j = a; j < b; j++)
+#define REPS(a, b, c) for (int i = a; i < b; i += c)
 
 #define inf 1000000000
 
