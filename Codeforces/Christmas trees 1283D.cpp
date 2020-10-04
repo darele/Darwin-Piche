9a7568dd8f485097b459befcefaae9fbeebeb7a0
darele <60158572+darele@users.noreply.github.com>
2020-10-03 21:13:04 -0600

    Update Christmas trees 1283D.cpp

--- Codeforces/Christmas trees 1283D.cpp
+++ Codeforces/Christmas trees 1283D.cpp
@@ -1,8 +1,7 @@
 /*
 https://codeforces.com/contest/1283/problem/D
 ir tomando los que estan a mi derecha y a mi izquierda (comenzando por los arboles)
-sacar la menor distancia (hacia el arbol de la derecha o al de la izquierda, de eso se encarga la priority queue
-siempre y cuando aplique bien las operaciones con el set arr)
+sacar la menor distancia eso lo hace la priority queue
 */
 
 #include <bits/stdc++.h>
@@ -27,11 +26,10 @@ int main() {
     int n, m;
     cin >> n >> m;
     ll a[n];
-    set <ll> kika, arr;
+    set <ll> kika;
     REP(0, n) {
         cin >> a[i];
         kika.insert(a[i]);
-        arr.insert(a[i]);
     }
     sort(a, a + n);
     priority_queue <dl, vector <dl>, greater <dl> > cola;
@@ -55,18 +53,9 @@ int main() {
         ans.push_back(u.second);
         sum += u.first;
         if (kika.find(u.second - 1) == kika.end()) {
-            auto temp = arr.upper_bound(u.second - 1);
-            if (temp != arr.begin()) {
-                temp--;
-                cola.push({abs(*temp - (u.second - 1)), u.second - 1});
-            }
             cola.push({u.first + 1, u.second - 1});
         }
         if (kika.find(u.second + 1) == kika.end()) {
-            auto temp = arr.upper_bound(u.second + 1);
-            if (temp != arr.end()) {
-                cola.push({abs((u.second + 1) - *temp), u.second + 1});
-            }
             cola.push({u.first + 1, u.second + 1});
         }
         kika.insert(u.second);
