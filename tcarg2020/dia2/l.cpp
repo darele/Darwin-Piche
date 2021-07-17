5368ad857dbc3f7abe680886260c2b945c51ea19
darele <darcai009@gmail.com>
2021-07-16 18:37:19 -0600

    Prblema l

---
+++ tcarg2020/dia2/l.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair <int, int>
+#define dl pair <ll, ll>
+#define vi vector <int>
+#define vl vector <ll>
+#define vii vector <ii>
+#define graph vector <vi>
+#define ff first
+#define ss second
+#define REP(a, b) for (int i = a; i < b; i++)
+#define REP2(a, b) for (int j = a; j < b; j++)
+#define REPS(a, b, c) for (int i = a; i < b; i += c)
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    cout << (n % 2 ? "black\n" : "white\n1 2");
+    return 0;
+}
